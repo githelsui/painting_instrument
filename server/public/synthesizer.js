01e963b8e65f07c65edd0606a4c247d7c746d8f2
githelsui <githelsuico@gmail.com>
2022-01-13 21:35:24 -0800

    synthesizer set up

--- server/public/synthesizer.js
+++ server/public/synthesizer.js
@@ -1,6 +1,29 @@
-var socket = io();
- 
+var socket = io(); 
+
+function playNote(note){
+    if(new String('C').valueOf() == new String(note).valueOf()){
+        playSynth(note);
+    }
+}
+
 socket.on('data', function(data){
-    console.log(data);
+    let note = data.substring(6, 7);
+    console.log("test " + note);
+    playNote(note);
     document.getElementById('title').innerHTML = data;
 }); 
+
+const vol = new Tone.Volume(+3).toDestination();
+const synth = new Tone.PolySynth().connect(vol).toMaster();
+
+function playSynth(note) {
+    console.log('NOTE IS PLAYED');
+    var pitch = note + "4";
+    console.log(pitch);
+    synth.triggerAttackRelease("C4", "4n");
+}
+
+document.querySelector('Starts Tone')?.addEventListener('Starts Tone', async () => {
+	await Tone.start();
+	console.log('audio is ready');
+})
