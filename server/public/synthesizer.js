b770435d0610b995192520e3a6e3059ad3ce1b8b
githelsui <githelsuico@gmail.com>
2022-01-13 22:12:00 -0800

    fix autoplay

--- server/public/synthesizer.js
+++ server/public/synthesizer.js
@@ -1,20 +1,28 @@
-var socket = io(); 
+const vol = new Tone.Volume(+3).toDestination();
+const synth = new Tone.PolySynth().connect(vol).toMaster();
+
+document.querySelector('button').addEventListener('click', async () => {
+    await Tone.start();
+    // your page is ready to play sounds
+    console.log("STAART");
+    readBoard();
+})
+
+function readBoard(){
+    var socket = io(); 
+    socket.on('data', function(data){
+        let note = data.substring(6, 7);
+        console.log("test " + note);
+        playNote(note);
+        document.getElementById('note').innerHTML = data;
+    }); 
+}
 
 function playNote(note){
     if(new String('C').valueOf() == new String(note).valueOf()){
         playSynth(note);
     }
 }
-
-socket.on('data', function(data){
-    let note = data.substring(6, 7);
-    console.log("test " + note);
-    playNote(note);
-    document.getElementById('title').innerHTML = data;
-}); 
-
-const vol = new Tone.Volume(+3).toDestination();
-const synth = new Tone.PolySynth().connect(vol).toMaster();
 
 function playSynth(note) {
     console.log('NOTE IS PLAYED');
@@ -23,7 +31,26 @@ function playSynth(note) {
     synth.triggerAttackRelease("C4", "4n");
 }
 
-document.querySelector('Starts Tone')?.addEventListener('Starts Tone', async () => {
-	await Tone.start();
-	console.log('audio is ready');
-})
+// function startTone(){
+//     console.log('audio is ready')
+//     if (Tone.context.state !== 'running') {
+//         Tone.context.resume();
+//         console.log("resumes");
+//     } else {
+//         console.log("starts");
+//         Tone.start();
+//     }
+// }
+
+// document.querySelector('Starts Tone')?.addEventListener('Starts Tone', async () => {
+//     // Tone.context.resume()
+// 	// await Tone.start()
+//     console.log('audio is ready')
+//     if (Tone.context.state !== 'running') {
+//         Tone.context.resume();
+//         console.log("resumes");
+//     } else {
+//         console.log("starts");
+//         await Tone.start();
+//     }
+// })
