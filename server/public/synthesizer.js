d6efd7023fdc9a1b3b0753a84ad1333a113710df
githelsui <githelsuico@gmail.com>
2022-01-14 20:26:07 -0800

    test with apple

--- server/public/synthesizer.js
+++ server/public/synthesizer.js
@@ -1,5 +1,6 @@
 const vol = new Tone.Volume(+3).toDestination();
 const synth = new Tone.PolySynth().connect(vol).toMaster();
+synth.set({ detune: -1200 });
 
 document.querySelector('button').addEventListener('click', async () => {
     await Tone.start();
@@ -12,15 +13,27 @@ document.querySelector('button').addEventListener('click', async () => {
 function readBoard(){
     var socket = io(); 
     socket.on('data', function(data){
+        let prevNote = data;
         let note = data.substring(6, 8);
         console.log("note: " + note);
+        // withApples(note);
         playSynth(note);
         document.getElementById('note').innerHTML = note;
     }); 
+    console.log("does it ever go here");
 }
 
 function playSynth(note) {
     console.log('NOTE IS PLAYED');
     console.log(note);
     synth.triggerAttackRelease(String(note), "50n");
+}
+
+function withApples(signal){
+    if(signal === "E4"){
+        synth.triggerAttackRelease(['D4', 'A4', 'C5'], "50n");
+    }
+    if(signal === "C5"){
+        synth.triggerAttackRelease(['C4', 'G4', 'B4'], "50n");
+    }
 }
